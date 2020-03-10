792bb9253d1ad87aec3c2b31fef73fdcb2e8760b
Ilia Nemankov <INemankov@luxoft.com>
2020-03-10 19:31:36 +0200

    Fix collecting options names

--- render_service_scripts/maya_batch_render.py
+++ render_service_scripts/maya_batch_render.py
@@ -49,8 +49,8 @@ def main():
     report = {{}}
     report['width'] = cmds.getAttr("defaultResolution.width")
     report['height'] = cmds.getAttr("defaultResolution.height")
-    report['min_samples'] = cmds.getAttr("redshiftOptions.unifiedMinSamples")
-    report['max_samples'] = cmds.getAttr("redshiftOptions.unifiedMaxSamples")
-    report['noise_threshold'] = cmds.getAttr("redshiftOptions.unifiedAdaptiveErrorThreshold")
+    report['min_samples'] = cmds.getAttr("RadeonProRenderGlobals.completionCriteriaMinIterations")
+    report['max_samples'] = cmds.getAttr("RadeonProRenderGlobals.completionCriteriaIterations")
+    report['noise_threshold'] = cmds.getAttr("RadeonProRenderGlobals.adaptiveThreshold")
     with open(os.path.join(".", "render_info.json"), 'w') as f:
         json.dump(report, f, indent=4)
