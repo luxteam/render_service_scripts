79055c2c51499f112dde8eb678f5afe8286bcbe2
Ilia Nemankov <INemankov@luxoft.com>
2020-03-11 21:02:32 +0200

    Move python3 command to python command

--- install/install_pylibs.bat
+++ install/install_pylibs.bat
@@ -1,4 +1,4 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
-python3 get-pip.py
-python3 -m pip install --upgrade pip wheel setuptools
-python3 -m pip install -r requirements.txt
+python get-pip.py
+python -m pip install --upgrade pip wheel setuptools
+python -m pip install -r requirements.txt
