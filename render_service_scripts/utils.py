54eee102976c964b8d24d4512955b64aa8d2ec23
GulDilin <zhenyagurin@gmail.com>
2020-03-20 16:04:24 +0300

    create Utils class with send_status function

---
+++ render_service_scripts/utils.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class Util:
+    def __init__(self, ip, logger):
+        self.ip = ip
+        self.logger = logger
+
+    def send_status(self, post_data, files=None):
+        try_count = 0
+        while try_count < 3:
+            try:
+                response = requests.post(self.ip, data=post_data, files=files) if files \
+                    else requests.post(self.ip, data=post_data)
+                if response.status_code == 200:
+                    self.logger.info("POST request successfuly sent.")
+                    break
+                else:
+                    self.logger.info("POST reques failed, status code: " + str(response.status_code))
+                    break
+            except Exception as e:
+                if try_count == 2:
+                    self.logger.info("POST request try 3 failed. Finishing work.")
+                    break
+                try_count += 1
+                self.logger.info("POST request failed. Retry ...")
